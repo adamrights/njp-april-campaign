13cbb2471431afc6ab7ba48e8b8b15b0d3d149c8
adam bedell <adamrights@gmail.com>
2013-02-14 00:13:30 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NjpAprilCampaign::Application.config.secret_token = '8be180fa0070c9a1fb95b723e2398ab825bafb2fb6368966cfb68d96fca0c0cb14039a150a0702973b5446d3e0dd2caebb5fb1b01a94ca53fbe69a2e1b206ad1'
